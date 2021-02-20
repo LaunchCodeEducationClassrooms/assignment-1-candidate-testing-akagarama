148c0a101a8cee3394bf12524a571282d01df953
akagarama <akagarama@gmail.com>
2021-02-20 02:36:37 +0000

    Change the code again

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,14 +8,13 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ","True or false: 5000 meters == 5 kilometers? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ","True or false: 5000 meters == 5 kilometers? ", "(5 + 3)/2 * 10 =? ", "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name: ");
+candidateName = input.question("What is your name?: ");
 }
 
 function askQuestion() {
@@ -36,30 +35,23 @@ for (let i = 0; i < correctAnswers.length; i++) {
     totalGrade++;
    } 
   }
-
   let grade = (totalGrade / questions.length) * 100;
   let candidateStatus = "";
   if (grade >= 80) {
     candidateStatus = "SUCCESS";
   } else {
     candidateStatus = "FAILED";
-  }
- 
-  console.log(`>>> Overall Grade: ${grade}% (${totalGrade} of ${questions.length} responses correct) <<<\n>>> Status: ${candidateStatus} <<<`)
-
-  return grade;
-  
+  } 
+  console.log(`>>> Overall Grade: ${grade}% (${totalGrade} of ${questions.length} responses correct) <<<\n>>> Status: ${candidateStatus} <<<`);
+  return grade;  
 }
-
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   console.log(`Hello: ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
-
-
+};
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
